62f096aee632b1bff161969b6ba26f480d3aaf12
chhunly <60867999+chhunlyyy@users.noreply.github.com>
2021-12-24 14:16:42 +0700

    added about us screen

---
+++ lib/screens/about_us/about_us_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:lottie/lottie.dart';
+import 'package:tinh/const/colors_conts.dart';
+import 'package:tinh/helper/widget_helper.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AboutUsScreen extends StatefulWidget {
+  const AboutUsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AboutUsScreen> createState() => _AboutUsScreenState();
+}
+
+class _AboutUsScreenState extends State<AboutUsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Material(
+        child: SafeArea(child: _buildBody(context)),
+      ),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    final String _phoneNumber = '0962030870';
+    return Column(
+      children: [
+        WidgetHelper.appBar(context, 'អំពីយើង'),
+        Expanded(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  _animationWidget(context),
+                  Text(
+                    'ទាក់ទងកាន់យើងតាមរយៈ',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: ColorsConts.primaryColor, fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text('លេខទូរស័ព្ទ'),
+                      Text('0962030870'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text('Gmail'),
+                      Text('chhunlyyy@gmail.com'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _animationWidget(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 3,
+      width: MediaQuery.of(context).size.width,
+      child: Lottie.asset('assets/lottie/about-us.json'),
+    );
+  }
+}
--- lib/screens/home_screen/components/drawer_menu_screen.dart
+++ lib/screens/home_screen/components/drawer_menu_screen.dart
@@ -2,6 +2,7 @@ import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:tinh/const/colors_conts.dart';
 import 'package:tinh/helper/navigation_helper.dart';
+import 'package:tinh/screens/about_us/about_us_screen.dart';
 import 'package:tinh/screens/login/login_screen/login.dart';
 import 'package:tinh/screens/logout/logout_screen.dart';
 import 'package:tinh/store/main/main_store.dart';
@@ -20,7 +21,7 @@ class DrawerMenuScreen extends StatelessWidget {
       color: ColorsConts.primaryColor,
       child: Column(
         children: [
-          _drawerItem(context, 'អំពីយើង', Icons.person_pin_circle_rounded, () {}),
+          _drawerItem(context, 'អំពីយើង', Icons.person_pin_circle_rounded, () => NavigationHelper.push(context, AboutUsScreen())),
           SizedBox(height: 15),
           !mainStore.userStore.isShopOwner
               ? _drawerItem(context, 'ចូល', Icons.login, () => NavigationHelper.push(context, LoginScreen(mainStore)))
